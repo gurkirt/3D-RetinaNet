605b4b89f18794e1750538e97c68b0cb0cfe19df
Gurkirt Singh <guru094@gmail.com>
2020-10-15 14:28:10 +0200

    u

--- models/backbone_models.py
+++ models/backbone_models.py
@@ -25,17 +25,17 @@ def backbone_models(args):
 
     model = resnetfpn(args)
 
-    # if args.MODE == 'train':
-    if MODEL_TYPE.startswith('RCN'):
-        model.identity_state_dict()
-    if MODEL_TYPE.startswith('RCGRU') or MODEL_TYPE.startswith('RCLSTM'):
-        model.recurrent_conv_zero_state()
+    if args.MODE == 'train':
+        if MODEL_TYPE.startswith('RCN'):
+            model.identity_state_dict()
+        if MODEL_TYPE.startswith('RCGRU') or MODEL_TYPE.startswith('RCLSTM'):
+            model.recurrent_conv_zero_state()
 
-    if not args.MODEL_PATH.endswith('.pth'):
-        load_dict = torch.load(args.MODEL_PATH + args.ARCH+'.pth')
-    else:
-        load_dict = torch.load(args.MODEL_PATH)
-    
-    model.load_my_state_dict(load_dict)
+        if not args.MODEL_PATH.endswith('.pth'):
+            load_dict = torch.load(args.MODEL_PATH + args.ARCH+'.pth')
+        else:
+            load_dict = torch.load(args.MODEL_PATH)
+        
+        model.load_my_state_dict(load_dict)
 
     return model
--- run_commands.py
+++ run_commands.py
@@ -1,12 +1,11 @@
 
 mode = 'gen_dets'
 import os
-
 for net,d in [('I3D',1),('RCN',1),('C2D',1)]:
     for train_subset in ['train_1', 'train_2','train_3']:
         for seq, bs, tseqs in [(16,8, [16,32]), (8,4,[8,32])]:
             for tseq in tseqs: 
                 if not (train_subset == 'train_1' and seq == 8):
-                    cmd = 'CUDA_VISIBLE_DEVICES={:d} python main.py --MODE={:s} --MODEL_TYPE={:s} --NMS_THRESH=0.5 --TEST_BATCH_SIZE=1 --TEST_SEQ_LEN={:d} --TRAIN_SUBSETS={:s} --SEQ_LEN={:d} --BATCH_SIZE={:d} \ &'.format(d, mode, net, tseq, train_subset, seq, bs)
+                    cmd = 'CUDA_VISIBLE_DEVICES={:d} python main.py --MODE={:s} --MODEL_TYPE={:s} --TEST_SEQ_LEN={:d} --TRAIN_SUBSETS={:s} --SEQ_LEN={:d} --BATCH_SIZE={:d}'.format(d, mode, net, tseq, train_subset, seq, bs)
                     print(cmd)
                     # os.system(cmd )
