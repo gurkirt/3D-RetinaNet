16e228642d25a75b8e5cc65ceb88590933c9c2d6
Salman Khan <32925691+salmank255@users.noreply.github.com>
2021-02-23 15:07:18 +0000

    Create get_kinetics_weights.sh

---
+++ kinetics-pt/get_kinetics_weights.sh
@@ -0,0 +1,5 @@
+
+#Downloading Kinetics weights
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=17xiC_Wrdv1noD9NZmgXGIZQWSnW0wnxP' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=17xiC_Wrdv1noD9NZmgXGIZQWSnW0wnxP" -O resnet50C2D.pth
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1XBMs4TLt2H378M_a0k23l8let-Ae2AlB' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1XBMs4TLt2H378M_a0k23l8let-Ae2AlB" -O resnet50I3D-NL.pth
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ZpbvJzvnDxJmKCFTs9wKmmA2qvm2aFBX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ZpbvJzvnDxJmKCFTs9wKmmA2qvm2aFBX" -O resnet50I3D.pth
